a6a2fa07ad4155318bdc5f53eb69fb6c303b7b9b
itsdevrajchauhan <rajchauhan6279@gmail.com>
2025-06-20 23:59:24 +0530

    C++  Course

---
+++ two.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+// This program is created by Raj Chauhan At C++ course
+int main()
+{
+    int sum = 4;
+    int ek = 3;
+    cout << "Hello this is Raj Chauhan " << sum << ek;
+    return 0;
+}
+
+/*
+'
+This Is multiline Comment
+
+*/
+
+// In this Tutorial i learn about Comment's ,Veriable and the
+// one cout << can output this << this << thiss << thiss << and this also
