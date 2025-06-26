8f5de9edabbc8505a7f8306f91f9ddc736491c03
itsdevrajchauhan <rajchauhan6279@gmail.com>
2025-06-26 16:49:50 +0530

    if else and switch case explained

---
+++ tut8.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int age;
+    cout << "Tell me Your Age :  " << endl;
+    cin >> age;
+    /* if ((age < 18) && (age > 0))
+      {
+          cout << "You are not eligible to come to party : " << endl;
+      }
+      else if (age == 18)
+      {
+          cout << "you can have kidd program : " << endl;
+      }
+      else if (age == 0)
+      {
+          cout << "you are not born: " << endl;
+      }
+      else
+      {
+          cout << "you can come to party : " << endl;
+      }
+
+      */
+
+    switch (age)
+    {
+    case 18:
+    {
+        cout << "you are 18" << endl;
+    }
+    break;
+
+    case 2:
+    {
+        cout << "you are not eligible" << endl;
+    }
+    /* code */
+    break;
+
+    default:
+    {
+        cout << "please enter valid age" << endl;
+    }
+    break;
+    }
+
+    return 0;
+}
+
+// sequence structure
+
+// selection sturctur //switch
+// loop structiure
